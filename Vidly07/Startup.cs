5c266cc060c2b8b62b37e708e161fb0ca3867b58
mohamed mohsen <mohamedmohsen2468@gmail.com>
2019-02-11 04:03:42 +0200

    Add project files.

---
+++ Vidly07/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly07.Startup))]
+namespace Vidly07
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
