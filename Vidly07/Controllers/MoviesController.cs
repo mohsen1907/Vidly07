8cb4fc6903cc11a4b1ca874d004483dde63d78c6
mohamed mohsen <mohamedmohsen2468@gmail.com>
2019-02-14 02:36:24 +0200

    Use a view model.

---
+++ Vidly07/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly07.Models;
+using Vidly07.ViewModels;
+
+namespace Vidly07.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+	        var movie = new Movie() {Name = "shrek" };
+			var customers = new List<Customer>
+			{
+				new Customer{Name = "Customer 1"},
+				new Customer{Name = "Customer 2"}
+			};
+
+	        var viewModel = new RandomMovieViewModel
+	        {
+				Movie = movie,
+				Customers = customers
+	        };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Vidly07/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly07.Models;
+
+namespace Vidly07.ViewModels
+{
+	public class RandomMovieViewModel
+	{
+		public Movie Movie { get; set; }
+		public List<Customer> Customers { get; set; }
+	}
+}
