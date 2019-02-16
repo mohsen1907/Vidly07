c25a4a8ca5d809f48827a2223661342e0b76b06d
mohamed mohsen <mohamedmohsen2468@gmail.com>
2019-02-16 16:59:09 +0200

    Enable Migrations.

--- Vidly07/Models/IdentityModels.cs
+++ Vidly07/Models/IdentityModels.cs
@@ -20,7 +20,8 @@ namespace Vidly07.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
+		public DbSet<Customer> Customers { get; set; } 
+		public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
