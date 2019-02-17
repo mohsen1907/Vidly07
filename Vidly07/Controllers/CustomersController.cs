4d9774718a823ac343cd25d1c8a9022ca31111cb
mohamed mohsen <mohamedmohsen2468@gmail.com>
2019-02-17 14:37:08 +0200

    Add a form to add/update a customer.

--- Vidly07/Controllers/CustomersController.cs
+++ Vidly07/Controllers/CustomersController.cs
@@ -12,17 +12,16 @@ namespace Vidly07.Controllers
 	public class CustomersController : Controller
 	{
 		private ApplicationDbContext _context;
-
-		public CustomersController()
-		{
-			_context = new ApplicationDbContext();
-		}
-
 		protected override void Dispose(bool disposing)
 		{
 			_context.Dispose();
 		}
 
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+	
 		public ViewResult Index()
 		{
 			var customers = _context.Customers.Include(c => c.MembershipType ).ToList();
@@ -39,7 +38,48 @@ namespace Vidly07.Controllers
 
 			return View(customer);
 		}
+		public ActionResult New()
+		{//Send to CustomerForm ViewModel
+			var membershiptypes = _context.MembershipTypes.ToList();
+			var viewModel = new CustomerFormViewModel()
+			{
+				MembershipTypes = membershiptypes
+			};
+			return View("CustomerForm",viewModel);
+		}
 
-		
+		[HttpPost]
+		public ActionResult Save(Customer customer)
+		{//receive from CustomerForm View
+			if (customer.Id == 0)
+			{
+				_context.Customers.Add(customer);
+			}
+			else
+			{
+				var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+				 
+				customerInDb.Name = customer.Name;
+				customerInDb.Birthdate = customer.Birthdate;
+				customerInDb.MembershipTypeId = customer.MembershipTypeId;
+				customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+			}
+			_context.SaveChanges();
+			return RedirectToAction("Index","Customers");
+		}
+
+		public ActionResult Edit(int Id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+			if (customer==null)
+				return HttpNotFound();
+			var viewModel = new CustomerFormViewModel
+			{
+				//it filled the Textboxes, because i sent the customer to Model
+				Customer = customer,
+				MembershipTypes = _context.MembershipTypes.ToList()
+			};
+			return View("CustomerForm",viewModel);
+		}
 	}
 }
---
+++ Vidly07/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly07.Models;
+
+namespace Vidly07.ViewModels
+{
+	public class CustomerFormViewModel
+	{
+		public IEnumerable<MembershipType> MembershipTypes { get; set; }
+		public Customer Customer { get; set; }
+	}
+}
